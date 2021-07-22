c06518bb50aee30004d421a4ee65fddd4a57db4b
assemblathe1 <taskinroman@mail.ru>
2021-07-22 20:45:34 +0500

    before reset

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
-
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-
-import static java.nio.charset.StandardCharsets.*;
 
 public class Main {
 
@@ -20,11 +16,25 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         HashMap<String, Path> referenceFixDirectories = getReferenceFixDirectories(reference);
-        ArrayList<Path> filesToConvert = copyThree(referenceFixDirectories, readyFix, hosts);
+        List<Path> filesToConvert = copyThree(referenceFixDirectories, readyFix);
         FileFixHandler(filesToConvert);
 
 
+    }
 
+    private static List<Path> copyThree(Map<String, Path> referenceFixDirectories, Path readyFix) {
+        List<String> hostList = Files.readAllLines(hostsList);
+        for (String host : hostList) {
+            source.forEach((key, value) -> {
+                try {
+                    Path dstPath = Paths.get(String.format("%s\\%s (%s)\\", readyFix, host, key));
+                    Files.createDirectory(dstPath);
+                    Files.walkFileTree((Path) value, new MyFileVisitor(dstPath));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
     }
 
     private static HashMap<String, Path> getReferenceFixDirectories(Path reference) throws IOException {
@@ -43,44 +53,53 @@ public class Main {
         return references;
     }
 
-    private static ArrayList<Path> copyThree(HashMap source, Path destination, Path hostsList) throws IOException {
 
-        List<String> hostList = Files.readAllLines(hostsList);
-        ArrayList<Path> filesToConvert = new ArrayList<>();
-        for (String host : hostList) {
-            source.forEach((key, value) -> {
-                try {
-                    Path dstPath = Paths.get(String.format("%s\\%s (%s)\\", readyFix, host, key));
-                    if (!Files.exists(dstPath)) {
-                        Files.createDirectory(dstPath);
-                    }
-                    filesToConvert.add(Paths.get(dstPath + "/" + "Report_New.csv"));
-                    Files.walkFileTree((Path) value, new FIleFixCopier(dstPath));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-        return filesToConvert;
-    }
-
-    private static void FileFixHandler(ArrayList<Path> filesToConvert) throws IOException {
+    private static void FileFixHandler(List<Path> filesToConvert) throws IOException {
         for (Path reportNewFile : filesToConvert) {
             Path readyCSV = Paths.get(String.format("%s\\%s.csv", readyREPORTS, reportNewFile.getParent().getFileName()));
-            if (Files.exists(readyCSV)) {
-               Files.delete(readyCSV);
-            }
-            Files.createFile(readyCSV);
+//            if (Files.exists(readyCSV)) {
+//                Files.delete(readyCSV);
+//            }
+//            Files.createFile(readyCSV);
             String text = new String(Files.readAllBytes(reportNewFile), "windows-1251");
+//            String text = Files.readString(reportNewFile, Charset.defaultCharset());
             List<String> lines = Arrays.stream(text.split(System.lineSeparator())).toList();
-
-            for (String line : lines) {
-                String[] values = line.split(",");
+            File file = new File(readyCSV.toString());
+//            FileOutputStream out = new FileOutputStream(file, true);
+//            PrintStream tempOut = System.out;
+            try (FileWriter writer = new FileWriter(file, Charset.defaultCharset(), false)) {
+//                PrintStream ps = new PrintStream(out);
+//                System.setOut(new PrintStream(out));
+                for (String line : lines) {
+                    String[] values = line.split(",");
 //                System.out.println(Arrays.toString(values));
-                System.out.println(values[1]);
-                List<String> list = Arrays.asList(values);
-                Files.write(readyCSV, list);
+//                    System.out.println(values);
+//                System.out.println(readyCSV);
+//                System.out.println(values.length);
+                    StringBuilder sb = new StringBuilder();
+                    for (String value : values) {
+//                    out.write(value.getBytes());
+//                    byte[] bs = value.getBytes();
+                        sb.append(value).append(" ");
+//                        writer.write(value);
+                        System.out.println(value);
+                    }
+                    writer.write(sb.toString().trim());
+                    System.out.println(sb);
+                    writer.write("\r\n");
+                }
+//                out.write(values[3].getBytes());
+
+
+//                FileWriter writer = new FileWriter(readyCSV.toString(), false);
+//                writer.write(values[1]);
+
+
+//                System.out.println(values[1]);
+//                List<String> list = Arrays.asList(values);
+//                Files.write(readyCSV, list);
             }
+//            System.setOut(tempOut);
         }
     }
 }
